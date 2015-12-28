bbe17309e0e1b38c00ec77907fa10f06fe81b5df
Daniel Hollenbeck <dan@hollenbecks.com>
2015-12-28 13:59:46 -0600

    working now

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # prorate
+
+Compute billing subscription upgrades, downgrades and prorated amounts. Uses Big.js to ensure accuracy of computations.
+
+## Installation
+
+```bash
+npm install prorate --save
+```
+
+## Usage
+
+When starting a subscription use the `amount` method to comute the amount to the bill the user.
+
+```javascript
+var prorate = require('prorate');
+var percent = prorate.monthPercentage(); //between 0 and 1
+var planPrice = 1.00;
+var amount = prorate.amount(percent, planPrie);
+```
+
+The usage for a subscription plan upgrade or downgrade is handled by the `change` method.
+
+```javascript
+var prorate = require('prorate');
+var percent = prorate.monthPercentage(); //between 0 and 1
+var planPrice1 = 1.00;
+var planPrice2 = 2.00;
+var amount = prorate.change(percent, planPrice1, planPrice2);
+```
+
+There is a convience method `monthPercentage` to aid in calculating the percentage of time.
+
+```javascript
+var prorate = require('prorate');
+
+// using current time
+var percentNow = prorate.monthPercentage();
+
+// using a date
+var date = new Date('December 17, 1995 03:24:00');
+var percentDate = prorate.monthPercentage(date);
+
+```
+
+## Code Quality
+```bash
+npm install -g eslint
+eslint .
+```
+
+## People
+
+The current maintainer is [Dan Hollenbeck](https://github.com/dhollenbeck).
+
+## LICENSE
+
+MIT
--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 'use strict';
 
 var Big = require('big.js');
+var Assert = require('assert');
+var isNumber = require('lodash.isnumber');
+var isDate = require('lodash.isdate');
+var isUndefined = require('lodash.isundefined');
 
 // ***** Floating Point Math *****
 // http://mikemcl.github.io/big.js/
-Big.DP = 2; //http://mikemcl.github.io/big.js/#dp
+Big.DP = 20; //http://mikemcl.github.io/big.js/#dp
 Big.RM = 1; //http://mikemcl.github.io/big.js/#rm
 
-exports.computeBad = function(percentOfTime, currentPlanPrice, newPlanPrice){
+exports.amount = function(percentOfTime, planPrice){
 
-	var credit, debit, prorated;
+	var amount, str;
 
-	credit = percentOfTime * currentPlanPrice;
-	debit = percentOfTime * newPlanPrice;
-	prorated = debit - credit;
+	//validate
+	Assert.ok(isNumber(percentOfTime), 'param `percentOfTime` must be a number.');
+	Assert.ok(percentOfTime >= 0, 'param `percentOfTime` must between 0 and 1.');
+	Assert.ok(percentOfTime <= 1, 'param `percentOfTime` must between 0 and 1.');
 
-	return prorated;
+	Assert.ok(isNumber(planPrice), 'param `planPrice` must be a number.');
+	Assert.ok(planPrice >= 0, 'param `planPrice` must be zero or greater.');
+
+	//calculate
+	amount = new Big(1);
+	amount = amount.sub(percentOfTime);
+	amount = amount.times(planPrice);
+
+	str = amount.toFixed(2);
+
+	return 1 * str;
 };
 
-exports.computeGood = function(percentOfTime, currentPlanPrice, newPlanPrice){
+exports.change = function(percentOfTime, currentPlanPrice, newPlanPrice){
 
-	var credit, debit, prorated;
+	var credit, debit, prorated, str;
 
+	//validate
+	Assert.ok(isNumber(percentOfTime), 'param `percentOfTime` must be a number.');
+	Assert.ok(percentOfTime >= 0, 'param `percentOfTime` must between 0 and 1.');
+	Assert.ok(percentOfTime <= 1, 'param `percentOfTime` must between 0 and 1.');
+
+	Assert.ok(isNumber(currentPlanPrice), 'param `currentPlanPrice` must be a number.');
+	Assert.ok(currentPlanPrice >= 0, 'param `currentPlanPrice` must be zero or greater.');
+
+	Assert.ok(isNumber(newPlanPrice), 'param `newPlanPrice` must be a number.');
+	Assert.ok(newPlanPrice >= 0, 'param `newPlanPrice` must be zero or greater.');
+
+	//setup
 	credit = new Big(currentPlanPrice);
 	debit = new Big(newPlanPrice);
 
@@ -29,14 +56,33 @@ exports.computeGood = function(percentOfTime, currentPlanPrice, newPlanPrice){
 	credit = credit.times(percentOfTime);
 	debit = debit.times(percentOfTime);
 	prorated = debit.sub(credit);
+	str = prorated.toFixed(2);
 
-	return prorated * 1; //convert from Big Number to primitive JS number
+	return 1 * str;
 };
 
-exports.test = function(){
-	var p = 0.5;
-	var p1 = 1.00;
-	var p2 = 2.00;
-	var prorated = exports.computeGood(p, p1, p2);
-	console.log('prorated', prorated);
+exports.monthPercentage = function(now){
+
+	//validate
+	Assert.ok(isDate(now) || isUndefined(now), 'param `now` must be a date or undefined.');
+
+	now = (now)? now : new Date();
+	var year = now.getFullYear();
+	var month = now.getMonth();
+	var date1 = new Date(year, month, 1, 0, 0 ,0);
+	var date2 = new Date(year, month + 1, 0, 23, 59, 59);
+	var percent = exports._percent(now, date1, date2);
+
+	return percent;
+};
+
+exports._percent = function(now, date1, date2){
+	var tsNow = now.getTime();
+	var ts1 = date1.getTime();
+	var ts2 = date2.getTime();
+
+	//calculate
+	var pct = new Big(tsNow - ts1);
+	pct = pct.div(ts2 - ts1);
+	return pct * 1; //convert from Big Number to primitive JS number
 };
